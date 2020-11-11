376ab02d8d535ea1c0e674d3567315e43ac36d3e
ivandemydko <ivandemydko@gmail.com>
2020-11-11 23:35:14 +0200

    Fix sql requests

--- src/main/java/com/example/prutest/services/EmployeeService.java
+++ src/main/java/com/example/prutest/services/EmployeeService.java
@@ -5,6 +5,7 @@ import com.example.prutest.mappers.EmployeesMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.List;
@@ -21,7 +22,7 @@ public class EmployeeService {
         log.info(String.valueOf(list));
         return list;
     }
-
+    
     public List<Employee> getTopFiveEmployees() {
         return employeesMapper.getTopFiveEmployees();
     }
